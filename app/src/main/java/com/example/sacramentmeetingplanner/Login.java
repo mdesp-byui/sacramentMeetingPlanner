cd955688287c96d8520ddb6315e1c01483f5434d
mdesp-byui <des20010@byui.edu>
2021-06-18 15:03:03 -0400

    First Commit

---
+++ app/src/main/java/com/example/sacramentmeetingplanner/Login.java
@@ -0,0 +1,86 @@
+package com.example.sacramentmeetingplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Login extends AppCompatActivity {
+    EditText emailAd, psword;
+    Button Registerbtn;
+    TextView loginbtn;
+    ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailAd = findViewById(R.id.Email);
+        psword = findViewById(R.id.Password);
+        Registerbtn = findViewById(R.id.Registerbton);
+        loginbtn = findViewById(R.id.loginbton);
+        progressBar = findViewById(R.id.progressBar);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailAd.getText().toString().trim();
+                String password = psword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailAd.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    emailAd.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    psword.setError("Password must be >=6 characters longer");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Authenticate the user
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sacramentmeetingplanner/Register.java
@@ -0,0 +1,98 @@
+package com.example.sacramentmeetingplanner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+
+
+public class Register extends AppCompatActivity {
+    EditText fullName,emailAd,psword;
+    Button registerBtn;
+    TextView loginBtn;
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fullName = findViewById(R.id.PersonName);
+        emailAd = findViewById(R.id.Email);
+        psword = findViewById(R.id.Password);
+        registerBtn = findViewById(R.id.Registerbton);
+        loginBtn = findViewById(R.id.loginbton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if (firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailAd.getText().toString().trim();
+                String password = psword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailAd.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    emailAd.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    psword.setError("Password must be >=6 characters longer");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Register the user in Firebase
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        }else{
+                            Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+    }
+}
