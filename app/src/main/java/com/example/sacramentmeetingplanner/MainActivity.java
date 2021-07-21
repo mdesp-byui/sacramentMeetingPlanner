1e166044dc18328a1ced47199fb005646e87c43c
mdesp-byui <des20010@byui.edu>
2021-07-21 11:37:29 -0400

    Touched up some layout issues

--- app/src/main/java/com/example/sacramentmeetingplanner/MainActivity.java
+++ app/src/main/java/com/example/sacramentmeetingplanner/MainActivity.java
@@ -2,22 +2,16 @@ package com.example.sacramentmeetingplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class MainActivity extends AppCompatActivity {
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
 
     public void opening(View view){
         Intent intent = new Intent(MainActivity.this, Agenda.class);
@@ -28,4 +22,11 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),Login.class));
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
 }
